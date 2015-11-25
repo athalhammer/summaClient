971f2d34070ecc5027d2e9fb2c8c785505f03c1f
Andreas Thalhammer <andreas.thalhammer@kit.edu>
2015-11-25 11:04:45 +0100

    new branch

---
+++ README.md
@@ -0,0 +1,62 @@
+# summaClient
+
+This JavaScript client implementation of the SUMMA API lets you integrate summaries from arbitrary SUMMA servers into your HTML documents.
+
+It supports two modes:
+
+- **SUMMA client** This mode lets you retrieve a summary from a specific entity that will be attached to a predefined DIV element in your page.
+- **qSum** This mode lets you annotate elements with the Internationalization Tag Set (ITS) property `its-ta-ident-ref` inside your HTML and summaries will be displayed as soon as you hover over the annotated elements.
+
+## Demo
+An integrated demo can be found at http://people.aifb.kit.edu/ath/summaClient/
+
+## Use
+Include the following lines in your HTML:
+
+- **SUMMA client**
+``` html
+<script src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
+<script src="http://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+<script src="http://people.aifb.kit.edu/ath/summaClient/js/summaClient.js"></script>
+<script>
+$(document).ready(function() {
+
+	// define which element should contain a specific summary
+	// parameters: entity, topK, language, fixed properties, (html) DIV-id, service
+	summa("http://dbpedia.org/resource/Quentin_Tarantino", 5, "en",
+		 "http://dbpedia.org/ontology/director,http://dbpedia.org/ontology/knownFor",
+		 "pf-summary", "http://km.aifb.kit.edu/summa/summarum");
+});
+</script>
+<!-- here is the element that should contain a specific summary -->
+<div class="sum" id="pf-summary"></div>
+```
+
+- **qSum**
+``` html
+<script src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
+<script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+<script src="http://people.aifb.kit.edu/ath/summaClient/js/summaClient.js"></script>
+<script>
+$(document).ready(function() {
+
+	// inline popups for annotated text
+	// parameters: topK, language, fixed properties, service
+	qSum(5, "en", null, "http://km.aifb.kit.edu/summa/summarum");
+
+});
+</script>
+The narrative sequence called "The Gold Watch" of 
+<span its-ta-ident-ref="http://dbpedia.org/resource/Pulp_Fiction">Pulp Fiction</span>
+ends with Butch picking up Fabienne with Zed's
+<span its-ta-ident-ref="http://dbpedia.org/resource/Chopper_(motorcycle)">chopper</span>
+```
+
+As a matter of fact, you can download the above *.js files respectively and include them on your own server.
+
+## Summaries
+You don't like the summaries of [Summarum](http://km.aifb.kit.edu/summa)? Build your own ones by implementing the interface `edu.kit.aifb.summa.Summarizer` of 
+https://github.com/athalhammer/summaServer.
+
+## License
+This source code is dual-licensed under the MIT license and GPL v3.
---
+++ js/summaClient.js
@@ -0,0 +1,120 @@
+/*!
+ * SUMMA Client JavaScript Library
+ * http://people.aifb.kit.edu/ath/summaClient/
+ *
+ * Includes jQuery and jQueryUI
+ * http://jquery.com/
+ * http://jqueryui.com/
+ *
+ * Copyright 2015 Andreas Thalhammer
+ * Released under the MIT license and GPL v3.
+ * https://github.com/athalhammer/summaClient/blob/master/LICENSE
+ *
+ * Date: 2015-08-28
+ */
+function summa(uri, topK, language, fixedProperty, id, service) {
+	$("#" + id).after("<div id=" + id + "_loading><img src='css/images/712.GIF'></div>");
+	$("#" + id + "_loading").hide();
+	$("#" + id).hide();
+	$.ajaxSetup({
+		accepts: {"json" : "application/rdf+json, application/json, text/javascript" },
+		contents: {"rdf+json" :  "application/rdf+json" },
+		converters: {"rdf+json json" : jQuery.parseJSON}
+	});
+	var url = service + "?entity=" + uri + "&topK=" + topK + "&maxHops=1";
+
+	if (language != null) {
+		url += "&language=" + language;
+	}
+	if (fixedProperty != null) {
+		url += "&fixedProperty=" + fixedProperty;
+	}
+	$.ajax({
+		dataType: "json",
+		url: url,
+        beforeSend: function() {
+		// show loading bar
+        	$("#" + id + "_loading").show();
+        },
+        complete: function() {
+		// remove loading bar
+        	$("#" + id + "_loading").remove();
+        },
+		success:
+		function (data) {
+			function label(uri) {
+				var part1 = data[uri];
+				if (part1 != null) {
+					return labels = part1["http://www.w3.org/2000/01/rdf-schema#label"][0]["value"];
+				} else {
+					var strArry = uri.split("/");
+					return strArry[strArry.length - 1];
+				}
+			}		
+		
+			var print = {"entity" : "", "statements" : []}
+			
+			var keys = Object.keys(data);
+			
+			for (i = 0; i < keys.length; i++) {
+				var types = data[keys[i]]["http://www.w3.org/1999/02/22-rdf-syntax-ns#type"];
+				if (types != null) {
+					if (types[0]["value"] == "http://purl.org/voc/summa/Summary") {
+						print["entity"] = data[keys[i]]["http://purl.org/voc/summa/entity"][0]["value"];
+					}
+					if (types[0]["value"] == "http://www.w3.org/1999/02/22-rdf-syntax-ns#Statement") {
+						var statement = {"subject" : "", "predicate" : "", "object" : ""};
+						statement["subject"] = data[keys[i]]["http://www.w3.org/1999/02/22-rdf-syntax-ns#subject"][0]["value"];
+						statement["predicate"] = data[keys[i]]["http://www.w3.org/1999/02/22-rdf-syntax-ns#predicate"][0]["value"];
+						statement["object"] = data[keys[i]]["http://www.w3.org/1999/02/22-rdf-syntax-ns#object"][0]["value"];
+						print.statements.push(statement);
+					}
+				}
+			}			
+			$("#" + id).append("<div style='float:right' id='" + id + "_close'>x</div><h2>" + label(print.entity) + "</h2><table></table>");
+			for (i = 0; i < print.statements.length; i++) {
+				if (print.statements[i].subject == print.entity) {
+					$("#" + id).children("table").append("<tr><td>" + label(print.statements[i].predicate) + "&nbsp;&nbsp;&nbsp;&nbsp;</td><td><a class=\"" + id + " " + "click\" id=\"" + print.statements[i].object + "\" href=\"#" + print.statements[i].object + "\">" + label(print.statements[i].object) + "</a></td></tr>");
+				} else if (print.statements[i].object == print.entity) {
+					$("#" + id).children("table").append("<tr><td>" + label(print.statements[i].predicate) + " of&nbsp;&nbsp;&nbsp;&nbsp;</td><td><a class=\"" + id + " " + "click\"id=\"" + print.statements[i].subject + "\" href=\"#" + print.statements[i].subject + "\">" + label(print.statements[i].subject) + "</a></td></tr>");
+				}
+			}
+			$("#" + id).show();
+			$("#" + id + "_close").click(function() {
+				$("#" + id).remove();
+			});
+			$('.' + id + '.click').click(function() {
+				$("#" + id).empty();
+				$("#" + id).hide();
+				summa(this.id, topK, language, fixedProperty, id, service);
+			});
+		}
+	});
+}
+
+
+function qSum(topK, lang, fixedproperty, service) {
+	var clicked = false;
+	$("[its-ta-ident-ref]").mouseover(function() {
+		var letter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+		var identifier = letter + Date.now();
+		$("body").append("<div class='sum sum-popup' id='" + identifier + "'></div>");
+		$("#" + identifier).position({
+			my: "left top",
+			at: "right",
+			of:  $(this),
+			collision: "fit"
+		});
+		summa($(this).attr("its-ta-ident-ref"), topK, lang, fixedproperty, identifier, service);
+	});
+	$("[its-ta-ident-ref]").click(function() {
+		clicked = true;
+	});
+	$("[its-ta-ident-ref]").mouseout(function() {
+		if (!clicked) {
+			$(".sum-popup").remove();
+		} else {
+			clicked = false;
+		}
+	});
+}
